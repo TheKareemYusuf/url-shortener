20854eb5459c989eb8cfd55dc39b580c888fa31f
Yusuf <yakande96@gmail.com>
2023-11-28 01:10:43 +0100

    implemented url shortener logic

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,15 +1,31 @@
 const URL = require("./../models/urlModel");
 
-const shortenUrl = (req, res, next) => {
-  // 1. Get the url from the body
+const shortenUrl = async (req, res, next) => {
+  try {
+    // 1. Get the url from the body
+    const url = req.body.originalUrl;
 
-  // 2. Shorten the url
+    // 2. Generate a unique id
+    function generateUrlId() {
+      return Math.random().toString(36).slice(2);
+    }
+    const newUrlId = generateUrlId();
 
-  // 3. Return the shortened url
-  res.status(201).json({
-    status: "success",
-    data: "url shortened successfully. Here's your new url ...",
-  });
+    // 3. Save both the unique id and url in the database
+    const newUrl = await URL.create({
+      originalUrl: url,
+      shortenedUrl: newUrlId,
+    });
+
+    // 4. Return the shortened url
+    res.status(201).json({
+      status: "success",
+      data: newUrl,
+      message: "url shortened successfully. Here's your new url ...",
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { shortenUrl };
